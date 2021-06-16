4905c241d19d89baeb6b798fe92b226aa546c8b7
erlmachinedev <erlmachinedev@gmail.com>
2021-06-17 00:09:53 +0300

    Adjusted debug

--- apps/aecore/src/aec_hard_forks.erl
+++ apps/aecore/src/aec_hard_forks.erl
@@ -62,7 +62,7 @@ ensure_env() ->
             application:set_env(aecore, fork, Fork);
         undefined ->
             ok
-    end catch E:R:S -> lager:debug("~n~p:ensure_env/0 E: ~p R: ~p S: ~p~n",[?MODULE, E, R,S ]) end.
+    end catch E:R:S -> lager:info("~n~p:ensure_env/0 E: ~p R: ~p S: ~p~n",[?MODULE, E, R,S ]) end.
 
 %% This function is supposed to be used only when:
 %% - a new block is being added to the database (in aec_conductor);
